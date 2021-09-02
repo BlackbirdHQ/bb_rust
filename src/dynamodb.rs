f421d1ef1fbb33afb341eeabd72e85bc20fe6566
Simon Vandel Sillesen <simon.vandel@gmail.com>
2021-09-02 14:12:28 +0200

    take as ref

--- src/dynamodb.rs
+++ src/dynamodb.rs
@@ -12,27 +12,27 @@ pub struct DynamoDbStreamEvent {
 /// Annoyingly, dynomite can not use the dynamodb_streams AttributeValue,
 /// so we need to do a convertion.
 pub fn dynamodb_stream_attrs_to_dynamodb_attrs(
-    attrs: HashMap<String, rusoto_dynamodbstreams::AttributeValue>,
+    attrs: &HashMap<String, rusoto_dynamodbstreams::AttributeValue>,
 ) -> HashMap<String, dynomite::AttributeValue> {
     attrs
         .into_iter()
-        .map(|(k, v)| (k, attribute_mapper(v)))
+        .map(|(k, v)| (k.clone(), attribute_mapper(v)))
         .collect()
 }
 
-fn attribute_mapper(v: rusoto_dynamodbstreams::AttributeValue) -> dynomite::AttributeValue {
+fn attribute_mapper(v: &rusoto_dynamodbstreams::AttributeValue) -> dynomite::AttributeValue {
     dynomite::AttributeValue {
-        s: v.s,
+        s: v.s.clone(),
         bool: v.bool,
-        n: v.n,
-        ns: v.ns,
+        n: v.n.clone(),
+        ns: v.ns.clone(),
         null: v.null,
-        m: v.m.map(dynamodb_stream_attrs_to_dynamodb_attrs),
-        l: v.l.map(|x| x.into_iter().map(attribute_mapper).collect()),
-        ss: v.ss,
+        m: v.m.clone().map(|x|dynamodb_stream_attrs_to_dynamodb_attrs(&x)),
+        l: v.l.clone().map(|x| x.into_iter().map(|a|attribute_mapper(&a)).collect()),
+        ss: v.ss.clone(),
         // Hack to convert between the two different versions of the `bytes` crate
-        b: v.b.map(|bytes| bytes.into_iter().collect()),
-        bs: v.bs.map(|vec_bytes| {
+        b: v.b.clone().map(|bytes| bytes.into_iter().collect()),
+        bs: v.bs.clone().map(|vec_bytes| {
             vec_bytes
                 .into_iter()
                 .map(|bytes| bytes.into_iter().collect())
