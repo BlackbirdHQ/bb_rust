c6cc641aa0d0ef8e871ae7951a1fe30670f4dcae
Simon Vandel Sillesen <simon.vandel@gmail.com>
2022-03-15 09:48:05 +0100

    generalize variables

--- examples/gateway_graphql_request.rs
+++ examples/gateway_graphql_request.rs
@@ -51,7 +51,7 @@ async fn main() -> Result<(), anyhow::Error> {
     let lambda = aws_sdk_lambda::Client::new(&aws_config::load_from_env().await);
 
     let raw_resp =
-        gateway_graphql_request::<serde_json::Value>(lambda, &graphql, function_name).await?;
+        gateway_graphql_request::<_, serde_json::Value>(&lambda, &graphql, function_name).await?;
     println!("{:?}", &raw_resp);
     Ok(())
 }
