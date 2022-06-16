8886cf4be3844095fd3a7c9cf6dd310b17d718fd
Martin Madsen <mj@blackbird.online>
2022-06-16 10:05:42 +0200

    Split services features into multiple for individual services

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,16 @@
 pub mod graphql;
 #[cfg(feature = "misc")]
 pub mod misc;
-#[cfg(feature = "services")]
+#[cfg(any(
+    feature = "services_cloudformation",
+    feature = "services_cognitoidentityprovider",
+    feature = "services_dynamodb",
+    feature = "services_lambda",
+    feature = "services_s3",
+    feature = "services_secretsmanager",
+    feature = "services_ssm",
+    feature = "services_sts"
+))]
 pub mod services;
 #[cfg(feature = "types")]
 pub mod types;
