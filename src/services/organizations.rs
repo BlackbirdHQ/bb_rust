ff238c4a2695d9147f5d827aeb1a55718e5e4a64
Martin Madsen <mj@blackbird.online>
2022-06-20 20:15:49 +0200

    Add Organizations AWS client

---
+++ src/services/organizations.rs
@@ -0,0 +1,15 @@
+use crate::services::in_region;
+use aws_sdk_organizations::Client as OrganizationsClient;
+use tokio::sync::OnceCell;
+
+async fn organizations_client(region: Option<&'static str>) -> OrganizationsClient {
+    OrganizationsClient::new(&in_region(region).await)
+}
+
+static ORGANIZATIONS: OnceCell<OrganizationsClient> = OnceCell::const_new();
+
+pub async fn organizations<'client>(region: Option<&'static str>) -> &'client OrganizationsClient {
+    ORGANIZATIONS
+        .get_or_init(|| organizations_client(region))
+        .await
+}
